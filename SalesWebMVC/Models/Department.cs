e24b157f9630f9855777288177a5f5a4582035f9
Merligus <gustavomerli18@gmail.com>
2022-02-09 19:35:29 -0300

    Other entities migration

--- SalesWebMVC/Models/Department.cs
+++ SalesWebMVC/Models/Department.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace SalesWebMVC.Models
 {
@@ -9,7 +8,27 @@ namespace SalesWebMVC.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
+        public Department()
+        {
+        }
 
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime init, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(init, final));
+        }
     }
 }
